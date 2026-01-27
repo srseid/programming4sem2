c770dac295c64ab9967789a9e078ae464a0ba8c1
srseid <148483902+srseid@users.noreply.github.com>
2026-01-26 21:46:11 -0500

    add subgraphs assignment

--- Assets/SicknessCheck.cs
+++ Assets/SicknessCheck.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 namespace NodeCanvas.Tasks.Actions {
 
 	public class SicknessCheck : ActionTask {
-        public BBParameter<float> sickness = 0;
-        public BBParameter<float> speed = 2;
-        public float sickCheck = 0f;
+        public BBParameter<float> sickness;
+        public BBParameter<float> speed;
+		public BBParameter<bool> poison;
+       
+		public MoldCheck moldScript;
+		
 
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
@@ -24,12 +27,16 @@ namespace NodeCanvas.Tasks.Actions {
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-            sickness.value += sickCheck * Time.deltaTime;
-			if (sickness.value > 5f)
+            sickness.value += sickness.value + moldScript.mold.value;
+			if (sickness.value >= 5f)
 			{
-				speed = 0;
+				speed = 0.25f;
 			}
 
+            if (sickness.value >= 10f)
+            {
+                poison = true;
+            }
         }
 
 		//Called when the task is disabled.
