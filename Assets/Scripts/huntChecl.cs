dc1b9f007514db2340df6ab6cf5ac5417ecaee66
srseid <148483902+srseid@users.noreply.github.com>
2026-02-17 02:28:38 -0500

    fixing fsm

--- Assets/Scripts/huntChecl.cs
+++ Assets/Scripts/huntChecl.cs
@@ -5,21 +5,24 @@ public class huntChecl : MonoBehaviour
     public bool seeGreen;
     public bool seePrey;
 
-    private void OnCollisionStay (Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
         Debug.Log("hi");
-        if(collision.gameObject.CompareTag("Grass"))
+        if (collision.gameObject.CompareTag("Grass"))
         {
             Debug.Log("grass!");
             seeGreen = true;
         }
         else { seeGreen = false; }
-
-        if (collision.gameObject.CompareTag("Player"))
+        if (seeGreen == true)
         {
-            Debug.Log("prey");
-            seePrey = true;
-        } else {  seePrey = false; }
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("prey");
+                seePrey = true;
+            }
+            else { seePrey = false; }
+        }
     }
 void onTriggerEnter (Collider other)
     {
